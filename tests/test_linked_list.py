478593c6d09f1fbece5d650ae0fef4f6ddd5f513
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-03-17 17:22:53 -0700

    test ll push, length and initialzation

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -2,12 +2,64 @@
 
 import sys
 import os
+import pytest
+
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
+
+
+@pytest.fixture
+def test_lists():
+    """Fixture for linked list tests."""
+    from src.linked_list import LinkedList
+    empty = LinkedList()
+    one = LinkedList(5)
+    multi = LinkedList([1, 2, 3, 4, 5])
+    return empty, one, multi
 
 
 def test_node_has_data():
     """Test node object has data."""
     from src.linked_list import Node
     ll = Node(5)
-    assert ll.data is 5
+    assert ll.data is 5 and ll.next is None
+
+
+def test_push_adds_to_the_head(test_lists):
+    """Test push, adds to the head of the list."""
+    ll = test_lists[1]
+    assert ll.head.data is 5
+
+
+def test_ll_has_head(test_lists):
+    """Test ll with iterable has a head."""
+    ll = test_lists[2]
+    assert ll.head.data is 5
+
+
+def test_empty_ll_head_none(test_lists):
+    """Test empty ll has a head of None."""
+    assert test_lists[0].head is None
+
+
+def test_size_one(test_lists):
+    """Test ll of one has length of one."""
+    assert test_lists[1]._length is 1
+
+
+def test_length_multi(test_lists):
+    """Test ll of multi has length."""
+    assert test_lists[2]._length is 5
+
+
+def test_empty_list_length(test_lists):
+    """Test empty list has no length."""
+    assert test_lists[0]._length is 0
+
+
+def test_when_push_increases_length(test_lists):
+    """Test push increases length."""
+    ll = test_lists[1]
+    length = ll._length
+    ll.push(3)
+    assert ll._length is length + 1
