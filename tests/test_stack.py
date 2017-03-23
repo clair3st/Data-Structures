442c6386190c476ec826d47b89b12ae515f6cbcc
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-03-22 18:14:54 -0700

    added pop tests

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -34,3 +34,21 @@ def test_stack_of_multiple_push(test_stack):
     """Test can push on an stack of multiple."""
     test_stack[2].push(2)
     assert test_stack[2]._stack.head.data is 2
+
+
+def test_empty_stack_pop(test_stack):
+    """Test can pop on an empty stack."""
+    with pytest.raises(IndexError):
+        test_stack[0].pop()
+
+
+def test_stack_of_one_pop(test_stack):
+    """Test can pop on an stack of 1."""
+    test_stack[1].pop()
+    assert test_stack[1]._stack.head is None
+
+
+def test_stack_of_multiple_pop(test_stack):
+    """Test can pop on an stack of multiple."""
+    test_stack[2].pop()
+    assert test_stack[2]._stack.head.data is 4
