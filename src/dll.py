39046c7ed6134cea59c7f0ec65b8856cb83c7e06
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-03-27 21:11:23 -0700

    fixing error in python 2

--- src/dll.py
+++ src/dll.py
@@ -15,11 +15,11 @@ class DoubleLinkedList(object):
     """Double linked list impplementation.
 
     Methods supported
-    push(val) - will insert the value ‘val’ at the head of the list
-    append(val) - will append the value ‘val’ at the tail of the list
+    push(val) - will insert the value (val) at the head of the list
+    append(val) - will append the value (val) at the tail of the list
     pop() - will pop the first val off the head of the list and return it.
     shift() - will remove the last val from the tail of the list and return it.
-    remove(val) - will remove the first instance of ‘val’ found in the list,
+    remove(val) - will remove the first instance of (val) found in the list,
     starting from the head.
     """
 
