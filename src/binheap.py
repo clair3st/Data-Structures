88950577cf2ca1f9cea132abbcb31369d849197a
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-05-10 12:19:47 -0700

    created a node

--- src/binheap.py
+++ src/binheap.py
@@ -1,1 +1,11 @@
 """Python implementation of Binary Heap."""
+
+
+class Node(object):
+    """Node object of binary heap."""
+
+    def __init__(self, parent, left=None, right=None):
+        """Initialize a node."""
+        self.left = left
+        self.right = right
+        self.parent = parent
