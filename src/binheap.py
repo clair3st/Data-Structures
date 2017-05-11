7761be2e0d93d74f5bb05a2a6d64209eacf9dd96
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-05-11 13:27:52 -0700

    added data to node

--- src/binheap.py
+++ src/binheap.py
@@ -4,8 +4,9 @@
 class Node(object):
     """Node object of binary heap."""
 
-    def __init__(self, parent, left=None, right=None):
+    def __init__(self, parent, data=None, left=None, right=None):
         """Initialize a node."""
         self.left = left
         self.right = right
         self.parent = parent
+        self.data = data
