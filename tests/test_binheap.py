1660daa9c7d48262352efab47dbd258aa752e2ec
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-05-11 15:36:07 -0700

    added another test for node

--- tests/test_binheap.py
+++ tests/test_binheap.py
@@ -10,3 +10,11 @@ def test_node_has_data():
     bh = Node(None, 5)
     assert bh.data is 5
     assert bh.left is None and bh.right is None
+
+
+def test_node_has_parent_and_children():
+    """Test node object has parent and children."""
+    from src.binheap import Node
+    bh = Node(None, 5)
+    assert bh.parent is None
+    assert bh.left is None and bh.right is None
