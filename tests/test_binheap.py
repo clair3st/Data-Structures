e046f849eaefd747c602a0fb7b6b06f67e007303
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-05-12 12:29:00 -0700

    added tests for adding to left and right children

--- tests/test_binheap.py
+++ tests/test_binheap.py
@@ -1,5 +1,15 @@
 """Testing binary Heap."""
 
+import pytest
+
+
+@pytest.fixture
+def heap():
+    """Fixture for heap."""
+    from src.binheap import Binheap
+    bh = Binheap()
+    return bh
+
 
 def test_node_has_data():
     """Test node object has data."""
@@ -17,9 +27,22 @@ def test_node_has_parent_and_children():
     assert node.left is None and node.right is None
 
 
-def test_push_val_to_head():
+def test_push_val_to_head(heap):
     """Test push first val adds to the head."""
-    from src.binheap import Binheap
-    bh = Binheap()
-    bh.push(3)
-    assert bh.head.data is 3
+    heap.push(3)
+    assert heap.head.data is 3
+
+
+def test_push_val_to_left(heap):
+    """Test push second val adds to the tree."""
+    heap.push(3)
+    heap.push(2)
+    assert heap.head.left.data is 2
+
+
+def test_push_val_to_right(heap):
+    """Test push second val adds to the tree."""
+    heap.push(10)
+    heap.push(2)
+    heap.push(5)
+    assert heap.head.right.data is 5
