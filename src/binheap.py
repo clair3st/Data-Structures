4bfd3e374446f7f380746c3561daf9920e6052f4
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-05-15 15:31:29 -0700

    reverted to list implementation

--- src/binheap.py
+++ src/binheap.py
@@ -1,17 +1,6 @@
 """Python implementation of Binary Heap."""
 
 
-class Node(object):
-    """Node object of binary heap."""
-
-    def __init__(self, data=None, parent=None, left=None, right=None):
-        """Initialize a node."""
-        self.left = left
-        self.right = right
-        self.parent = parent
-        self.data = data
-
-
 class Binheap(object):
     """Python implementation of binary heap.
 
@@ -23,23 +12,11 @@ class Binheap(object):
 
     def __init__(self, data=None):
         """Initialize bin heap."""
-        self.head = None
+        self.container = [None]
 
     def push(self, val):
         """Put a new value into the heap."""
-        if not self.head:
-            self.head = Node(val)
-        else:
-            curr = self.head
-            while True:
-                if curr.left and curr.right:
-                    curr = curr.left
-                elif curr.left:
-                    curr.right = Node(val)
-                    break
-                else:
-                    curr.left = Node(val)
-                    break
+        self.container.append(val)
 
     def pop(self):
         """Remove the top value of the heap."""
