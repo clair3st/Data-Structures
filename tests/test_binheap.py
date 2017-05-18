f6e6304abb4e69efcf031d507eea2a33c513ef49
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-05-17 20:45:07 -0700

    added test to use push method

--- tests/test_binheap.py
+++ tests/test_binheap.py
@@ -4,21 +4,30 @@ import pytest
 
 
 @pytest.fixture
-def heap():
-    """Fixture for heap."""
+def empty_heap():
+    """Fixture for empty_heap."""
     from src.binheap import Binheap
     bh = Binheap()
     return bh
 
 
-def test_push_val_to_head(heap):
+def test_push_val_to_head(empty_heap):
     """Test push first val adds to the head."""
-    heap.push(3)
-    assert heap.container == [None, 3]
+    empty_heap.push(3)
+    assert empty_heap.container == [None, 3]
 
 
-def test_push_val_to_left(heap):
+def test_push_val(empty_heap):
     """Test push second val adds to the tree."""
-    heap.push(3)
-    heap.push(2)
-    assert heap.container == [None, 2, 3]
+    empty_heap.push(3)
+    empty_heap.push(2)
+    assert empty_heap.container == [None, 3, 2]
+
+
+def test_push_val_large(empty_heap):
+    """Test push val for larger number."""
+    empty_heap.push(3)
+    empty_heap.push(2)
+    empty_heap.push(1)
+    empty_heap.push(16)
+    assert empty_heap.container == [None, 16, 3, 1, 2]
