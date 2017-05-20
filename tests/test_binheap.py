e107eb48dcbd07694f15439ced542f8299e70a65
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-05-19 20:05:49 -0700

    added test for initializing with a list

--- tests/test_binheap.py
+++ tests/test_binheap.py
@@ -11,6 +11,14 @@ def empty_heap():
     return bh
 
 
+@pytest.fixture
+def heap():
+    """Fixture for a heap."""
+    from src.binheap import Binheap
+    bh = Binheap([10, 4, 2, 6, 13, 72, 1, 49])
+    return bh
+
+
 def test_push_val_to_head(empty_heap):
     """Test push first val adds to the head."""
     empty_heap.push(3)
@@ -37,3 +45,8 @@ def test_push_on_empty(empty_heap):
     """Test push on an empty list."""
     empty_heap.push(1)
     assert empty_heap.container == [None, 1]
+
+
+def test_initialize_iterable(heap):
+    """Test heap can be initialized with iterable."""
+    assert heap.container == [None, 72, 49, 13, 10, 6, 2, 1, 4]
