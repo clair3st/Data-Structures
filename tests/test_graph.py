a3065d9c3413dc1bedaac2aeb709e6fabd1792f6
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-06-01 15:14:25 -0700

    added test for add node

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -26,3 +26,15 @@ def test_nodes_one(test_graph):
 def test_nodes_graph(test_graph):
     """Test nodes method list."""
     assert sorted(test_graph[2].nodes()) == ['A', 'B', 'C', 'D', 'E']
+
+
+def test_add_nodes(test_graph):
+    """Test add node adds a node."""
+    test_graph[0].add_node('A')
+    assert test_graph[0].nodes() == ['A']
+
+
+def test_add_nodes_dont_duplicate(test_graph):
+    """Test add node doesn't duplicate."""
+    test_graph[1].add_node('A')
+    assert test_graph[1].nodes() == ['A']
