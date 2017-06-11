784a9d0986b00347ff14154b297a5d6b6e8fb648
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-06-10 22:26:56 -0700

    added methods to class priorityq

---
+++ src/priorityq.py
@@ -0,0 +1,31 @@
+"""Python implementation of a priorityq."""
+
+
+class PriorityQ(object):
+    """
+    Priority Q data structure.
+
+    Following methods are supported.
+
+    Insert(value): inserts a value into the queue. 
+    Takes an optional argument for that value's priority.
+    pop(): removes the most important item from the queue
+    and returns its value.
+    peek(): returns the most important item without removing it from the queue.
+    """
+
+    def __init__(self, data):
+        """Initialize priorityq."""
+        pass
+
+    def insert(self, val, priority=0):
+        """Insert a val into the queue with an argument for the priority."""
+        pass
+
+    def pop(self):
+        """Removes the most important item from the queue."""
+        pass
+
+    def peek(self):
+        """Returns the most important item without removing it."""
+        pass
