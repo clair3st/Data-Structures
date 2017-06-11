50eef431e365d83459b4c8ae8424002dadb7eeb4
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-06-10 22:32:10 -0700

    added init and insert method

--- src/priorityq.py
+++ src/priorityq.py
@@ -1,5 +1,7 @@
 """Python implementation of a priorityq."""
 
+from a_queue import Queue
+
 
 class PriorityQ(object):
     """
@@ -7,25 +9,25 @@ class PriorityQ(object):
 
     Following methods are supported.
 
-    Insert(value): inserts a value into the queue. 
+    Insert(value): inserts a value into the queue.
     Takes an optional argument for that value's priority.
     pop(): removes the most important item from the queue
     and returns its value.
     peek(): returns the most important item without removing it from the queue.
     """
 
-    def __init__(self, data):
+    def __init__(self):
         """Initialize priorityq."""
-        pass
+        self._container = Queue()
 
     def insert(self, val, priority=0):
         """Insert a val into the queue with an argument for the priority."""
-        pass
+        self._container.push((val, priority))
 
     def pop(self):
-        """Removes the most important item from the queue."""
+        """Remove the most important item from the queue."""
         pass
 
     def peek(self):
-        """Returns the most important item without removing it."""
+        """Return the most important item without removing it."""
         pass
