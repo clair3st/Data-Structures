91a20ce86e41066821cb987ea54dbf51da9014c8
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-06-28 20:58:26 -0700

    working on insert, making a helper function for creating children and working on search function

--- src/bst.py
+++ src/bst.py
@@ -4,18 +4,20 @@
 class Node(object):
     """Node, or leaf of the BST."""
 
-    def __init__(self, val=None):
+    def __init__(self, val=None, parent=None):
         """Create node object."""
         self.val = val
         self.right = None
         self.left = None
-        self.parent = None
+        self.parent = parent
+        self.balance = 0
 
 
 class Bst(object):
     """Binary Search Tree.
 
     Binary Search tree supports the following methods:
+
     insert(val): will insert the value val into the BST. If val is
      already present, it will be ignored.
 
@@ -43,11 +45,45 @@ class Bst(object):
         self._size = 0
         self._rbalance = 0
         self._lbalance = 0
+        self._depth = 0
         self.root = None
+
+    def insert(self, val):
+        """Insert val into BST. If val is already present will be ignored."""
+        if not self.root:
+            self.root = Node(val)
+            self.size += 1
+            return
+
+        curr = self.root
+        while curr:
+            if val < curr.val:
+                curr = self._set_child(curr, 'left', val)
+            elif val > curr.val:
+                curr = self._set_child(curr, 'right', val)
+            else:
+                break
+
+    def _set_child(self, curr, side, val):
+        """Helping."""
+        if getattr(curr, side):
+            curr = getattr(curr, side)
+        else:
+            setattr(curr, side, Node(val, curr))
+            self._size += 1
+        return curr
 
     def search(self, val):
         """Return the node containing val."""
-        pass
+        curr = self.root
+        while curr:
+            if curr.val == val:
+                return curr
+            elif curr.val < val:
+                curr = curr.left
+            else:
+                curr = curr.right
+
 
     def size(self):
         """Return the size of the BST."""
