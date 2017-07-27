a4a75601d9444f69f6577fb046c945db571fc172
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-07-27 14:15:08 -0700

    wrote test for pre order traversal

--- tests/test_bst.py
+++ tests/test_bst.py
@@ -16,6 +16,20 @@ def test_bsts():
     return empty, one, three, balance, leftheavy, rightheavy
 
 
+@pytest.fixture
+def test_traversals():
+    """Fixture for traversals."""
+    from src.bst import Bst
+    fixture = {
+        'tree': Bst(['F', 'B', 'A', 'D', 'C', 'E', 'G', 'I', 'H']),
+        'pre_order': ['F', 'B', 'A', 'D', 'C', 'E', 'G', 'I', 'H'],
+        'in_order': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
+        'post_order': ['A', 'C', 'E', 'D', 'B', 'H', 'I', 'G', 'F'],
+        'breadth': ['F', 'B', 'G', 'A', 'D', 'I', 'C', 'E', 'H']
+    }
+    return fixture
+
+
 def test_node_is_leaf(test_bsts):
     """Test node is leaf bst."""
     assert test_bsts[1].root.is_leaf()
@@ -81,3 +95,9 @@ def test_balance_method(test_bsts):
 def test_search_method_node_exists(test_bsts):
     """Test search method for a node that exists."""
     assert all(tree.search(5) for tree in test_bsts[1:])
+
+
+def test_pre_order(test_traversals):
+    """Test preorder for a traversal."""
+    path = [i for i in test_traversals['tree'].pre_order()]
+    assert path == test_traversals['pre_order']
