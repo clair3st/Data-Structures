afc3613fc237f4d424c94a7c1566bed7f34daf72
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-07-27 12:22:01 -0700

    added tests for search, balance and depth

--- tests/test_bst.py
+++ tests/test_bst.py
@@ -10,10 +10,15 @@ def test_bsts():
     empty = Bst()
     one = Bst([5])
     three = Bst([5, 3, 7])
-    balance = Bst([5, 3, 2, 1, 9, 7, 8])
+    balance = Bst([5, 3, 2, 4, 9, 7, 10])
     leftheavy = Bst([5, 3, 2, 1])
     rightheavy = Bst([5, 6, 7, 8, 9, 10])
     return empty, one, three, balance, leftheavy, rightheavy
+
+
+def test_node_is_leaf(test_bsts):
+    """Test node is leaf bst."""
+    assert test_bsts[1].root.is_leaf()
 
 
 def test_insert_sets_root(test_bsts):
@@ -58,6 +63,21 @@ def test_contains_method_no_val(test_bsts):
     """Test contains that doesnt exist."""
     assert not test_bsts[4].contains(10)
 
-# def test_depth_method(test_bsts):
-#     """Test depth method."""
-#     assert test_bsts[2].depth() == 2
+
+def test_depth_method(test_bsts):
+    """Test depth method."""
+    depths = [None, 1, 2, 3, 4, 6]
+    assert all(tree.depth() == depths[idx]
+               for idx, tree in enumerate(test_bsts))
+
+
+def test_balance_method(test_bsts):
+    """Test the balance method."""
+    balance = [0, 0, 0, 0, -3, 5]
+    assert all(tree.balance() == balance[idx]
+               for idx, tree in enumerate(test_bsts))
+
+
+def test_search_method_node_exists(test_bsts):
+    """Test search method for a node that exists."""
+    assert all(tree.search(5) for tree in test_bsts[1:])
